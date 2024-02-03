d7b3780b3f9970f899da0f3c43acaadf90aef155
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-03 11:57:23 -0600

    Write new test block

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,7 +1,14 @@
 import { expect } from 'chai';
+import { exampleFunction1, exampleFunction2 } from '../src/domUpdates';
 
 describe('User Repository', () => {
   it('should run tests', function () {
-    expect(true).to.be(true);
+    expect(true).to.be.true;
+  });
+
+  it('should be a function', function() {
+
+    expect(exampleFunction1).to.be.a('function')
+    expect(exampleFunction1('Tayla')).to.equal('oh hi there Tayla')
   });
 });
