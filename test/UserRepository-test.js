5075bdbbc82a23e5e33bb69f98e5432d50814b6f
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-05 14:46:52 -0600

    Import scripts test function

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { exampleFunction1, exampleFunction2 } from '../src/domUpdates';
+// import { testThis } from '../src/scripts';
 
 describe('User Repository', () => {
   it('should run tests', function () {
@@ -11,4 +12,9 @@ describe('User Repository', () => {
     expect(exampleFunction1).to.be.a('function')
     expect(exampleFunction1('Tayla')).to.equal('oh hi there Tayla')
   });
+
+  // it('should be a function', function() {
+
+  //   expect(testThis).to.be.a('function')
+  // });
 });
