6fdf5c633f186745ed08d2192646a65f5f8a45d3
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-05 21:13:44 -0600

    Add section comments

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,16 +2,19 @@ import { getUserInfo, getAverageSteps } from '../src/user-info';
 import userData from './data/users'; 
 const users = userData.users
 
-
+//QUERY SELECTORS
 const nameDisplay = document.querySelector('h1')
 const addressEmail = document.querySelector('#address-email')
 const stepsStride = document.querySelector('#steps-stride')
 const averageStepDisplay = document.querySelector('h3')
 
+//GLOBAL VARIABLES
 const randomUser = getUserInfo(Math.floor(Math.random() * users.length), users)
 
+//EVENT LISTENERS
 window.addEventListener('load', displayUserInfo)
 
+//FUNCTIONS
 function displayUserInfo() {
   displayPersonalInfo();
   displayStepComparison();
