b0d8f928211e9b64782c08b3d44af930e9c2622f
Erin Kelley <erin.kelley01@gmail.com>
2024-02-05 18:53:21 -0700

    Add function to display average steps and compare to specific user

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,5 +6,30 @@ const addressEmail = document.querySelector('#address-email')
 const stepsStride = document.querySelector('#steps-stride')
 const averageSteps = document.querySelector('h3')
 
+window.addEventListener('load', displayUserInfo)
+
+
 const randomUser = getUserInfo(Math.floor(Math.random() * users.length))
 
+function displayUserInfo() {
+  displayPersonalInfo();
+  displayStepComparison();
+}
+
+function displayPersonalInfo() {
+  nameDisplay.innerText = randomUser.name;
+  addressEmail.innerText = `${randomUser.address} <br></br> ${randomUser.email}` 
+  stepsStride.innerText = `${randomUser.strideLength} <br></br> ${randomUser.dailyStepGoal}` 
+}
+
+function displayStepComparison() {
+  let averageSteps = getAverageSteps(users);
+  let differenceInSteps = Math.abs(averageSteps - randomUser.dailyStepGoal); 
+  if(averageSteps > randomUser.dailyStepGoal) {
+    averageSteps.innerText = `Your step goal was ${differenceInSteps} less than the average.`
+  } else if (averageSteps < randomUser.dailyStepGoal){
+    averageSteps.innerText = `Your step goal was ${differenceInSteps} more than the average!`
+  } else {
+    averageSteps.innerText = `Your step goal was equal to the average, congrats!`
+  }
+}
