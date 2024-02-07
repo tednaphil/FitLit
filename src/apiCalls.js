1051c69bb16dd371a5a9ae19f685c5d5562124cf
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-06 20:43:35 -0600

    Write fetch request for user data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,15 @@
 // Your fetch requests will live here!
 
 
-console.log('I will be a fetch request!')
+// console.log('I will be a fetch request!')
 
+fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+    .then(response => response.json())
+    .then(data => {
+        data.users.forEach((user) => {
+            userData.push(user)
+        })
+    });
+
+let userData = []
+// console.log('user data: ', userData)
