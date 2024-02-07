98eb692ebe11e708040a8ff2a4cea3c9d24f241d
Erin Kelley <erin.kelley01@gmail.com>
2024-02-06 20:47:28 -0700

    Refactor DOM functions to work with fetch API calls

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -11,27 +11,45 @@ const stepsStride = document.querySelector('#steps-stride')
 const averageStepDisplay = document.querySelector('h3')
 const hydrationWeek = document.querySelector('#hydro-week')
 
-
-//GLOBAL VARIABLES
-const randomUser = getUserInfo(Math.floor(Math.random() * users.length), users)
-
-//EVENT LISTENERS
-window.addEventListener('load', displayAllInfo)
-
-//FUNCTIONS
-function displayAllInfo() {
-  displayPersonalInfo();
-  displayStepComparison();
-  displayHydrationInfo();
+const allData = [
+  fetch(`https://fitlit-api.herokuapp.com/api/v1/users`),
+  fetch(`https://fitlit-api.herokuapp.com/api/v1/sleep`),
+  fetch(`https://fitlit-api.herokuapp.com/api/v1/hydration`)
+]
+function fetchData() {
+Promise.all(allData)
+  .then((res) => {
+    Promise.all(res.map((item) => {
+    return item.json();
+    }))
+    .then(([info, sleep, hydration]) => {
+      const randomUser = getUserInfo(Math.floor(Math.random() * info.users.length), info.users)
+      console.log(randomUser)
+      displayPersonalInfo(randomUser)
+      console.log(getAverageSteps(info.users))
+      displayStepComparison(randomUser)
+      displayHydrationInfo(randomUser, hydration)
+      console.log(sleep.sleepData);
+      console.log(hydration.hydrationData)
+    })
+  })
+  .catch(error => {
+    console.log("error")
+    return error; 
+  })
 }
 
-function displayPersonalInfo() {
+//EVENT LISTENERS
+window.addEventListener('load', fetchData)
+
+// FUNCTIONS
+function displayPersonalInfo(randomUser) {
   nameDisplay.innerText = randomUser.name;
   addressEmail.innerHTML = `${randomUser.address} <br></br> ${randomUser.email}` 
   stepsStride.innerHTML = `Stride Length: ${randomUser.strideLength}<br></br>Daily Step Goal: ${randomUser.dailyStepGoal}` 
 }
 
-function displayStepComparison() {
+function displayStepComparison(randomUser) {
   let averageSteps = getAverageSteps(users);
   let differenceInSteps = Math.abs(averageSteps - randomUser.dailyStepGoal); 
   if(averageSteps > randomUser.dailyStepGoal) {
@@ -43,7 +61,7 @@ function displayStepComparison() {
   }
 }
 
-function displayHydrationInfo() {
+function displayHydrationInfo(randomUser, hydrationData) {
   const todayInfo = findIntakeWeek(randomUser.id, hydrationData.hydrationData)
   hydrationWeek.innerHTML = `Today: ${todayInfo[0].numOunces} ounces`
   for (var i = 1; i < todayInfo.length; i++) {
