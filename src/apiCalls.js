a8061034ca42288290031812554c25d56cff3e60
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-06 22:42:02 -0600

    Export getData function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,13 +3,52 @@
 
 // console.log('I will be a fetch request!')
 
-fetch('https://fitlit-api.herokuapp.com/api/v1/users')
-    .then(response => response.json())
-    .then(data => {
-        data.users.forEach((user) => {
-            userData.push(user)
-        })
-    });
+// fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+//     .then(response => response.json())
+//     // .then(data => data)
+//     // .then(userArray => console.log(userArray))
+//     // .then(data => console.log('response data: ', data))
+//     .then(data => {
+//         data.users.forEach((user) => {
+//             users.push(user)
+//         })
+//     });
 
-let userData = []
-// console.log('user data: ', userData)
+// export const users = []
+// console.log('user data: ', users)
+
+function getUsers() {
+    return fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+}
+
+function getHydration() {
+    return fetch('https://fitlit-api.herokuapp.com/api/v1/hydration')
+}
+
+
+
+// const data = Promise.all([getUsers(), getHydration()])
+//     .then(values => Promise.all(values.map( value => value.json())))
+//     .then(parsed => parsed)
+
+function getData() {
+    return Promise.all([getUsers(), getHydration()])
+    .then(values => Promise.all(values.map( value => value.json())))
+    .then(data => {
+        let formattedData = {};
+        data.forEach((element) => {
+            // console.log('element', Object.keys(element))
+            formattedData[Object.keys(element)] = element
+        })
+        return formattedData
+    })
+    // .then(parsed => parsed)
+}
+
+// const data = getData()
+// console.log('data', data)
+
+export {
+    getData
+}
+
