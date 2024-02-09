4b00b54b8ce144ef56644c673912186e09a8f485
Erin Kelley <erin.kelley01@gmail.com>
2024-02-08 21:19:52 -0700

    Add semi-colons to functions in testing suite

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -4,38 +4,38 @@ import { calculateAverageIntake, findIntakeByDay, findIntakeWeek } from '../src/
 
 describe('calculate average intake', function() {
   it('should be a function', function() {
-    expect(calculateAverageIntake).to.be.a("function")
+    expect(calculateAverageIntake).to.be.a("function");
     });
 
   it('return the user’s average fluid intake for all time', function() {
-    const userIntake = calculateAverageIntake(1, testHydrationData) 
+    const userIntake = calculateAverageIntake(1, testHydrationData);
     
     expect(userIntake).to.equal(53);
-  })
-})
+  });
+});
 
 describe('report intake by day', function() {
   it('should be a function', function() {
-    expect(findIntakeByDay).to.be.a("function")
+    expect(findIntakeByDay).to.be.a("function");
     });
 
   it('return the user’s fluid intake for a specific day', function() {   
-    const intake1 = findIntakeByDay(1, "2023/04/01", testHydrationData)
-    const intake2 = findIntakeByDay(2, "2023/04/02", testHydrationData)
+    const intake1 = findIntakeByDay(1, "2023/04/01", testHydrationData);
+    const intake2 = findIntakeByDay(2, "2023/04/02", testHydrationData);
     
     expect(intake1).to.equal(88);
     expect(intake2).to.equal(64);
   });
-})
+});
 
 describe('report intake by week', function() {
   it('should be a function', function() {
-    expect(findIntakeWeek).to.be.a("function")
+    expect(findIntakeWeek).to.be.a("function");
     });
 
   it('should return information about daily intake over past week', function() {   
-    const intake1 = findIntakeWeek(1, testHydrationData)
-    const intake2 = findIntakeWeek(2, testHydrationData)
+    const intake1 = findIntakeWeek(1, testHydrationData);
+    const intake2 = findIntakeWeek(2, testHydrationData);
 
     expect(intake1.length).to.equal(7);
     expect(intake2.length).to.equal(7);
@@ -53,7 +53,7 @@ describe('report intake by week', function() {
     });
   });
   it('should still return partial data if user has less than seven entries', function(){
-    const intake3 = findIntakeWeek(3, testHydrationData)
+    const intake3 = findIntakeWeek(3, testHydrationData);
 
     expect(intake3.length).to.equal(4);
   });
