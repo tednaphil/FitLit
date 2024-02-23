5af09241cea8cd48c9f61943f6b7089925bd7c2a
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-02-22 20:40:53 -0600

    Add profile icon and prelim aside elements

--- src/scripts.js
+++ src/scripts.js
@@ -5,6 +5,7 @@ import './css/styles.css';
 import './images/background-space.jpg';
 import './images/txt-icon.png';
 import './images/graph-icon.png';
+import './images/profile-icon.png';
 import './images/step-party.png';
 
 
